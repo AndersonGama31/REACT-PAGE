f4bddfbacee7939be75f3df736905722e90f0bc5
AndersonGama31 <andersongama31@gmail.com>
2022-06-04 18:05:48 -0300

    Resolução de bugs e finalização da página

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-        
+        <Routes>
+          
+          <Route exact path="/" element={<Home />}/>
+
+        </Routes>
       </Router>
     </>
   );
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,26 +2,33 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import videos from '../videos/video-1.mp4'
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src="/video/video-2.mp4" autoPlay loop muted />
+      <video src={videos} autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
-      <div className="hero-btn">
-      <Button className="btns" 
-      buttonStyle='btn--outline'
-      buttonSize='btn--large'>
-        Get started
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
         </Button>
-        <Button className="btns" 
-      buttonStyle='btn--primary'
-      buttonSize='btn--large'>
-        WATCH TRAILER <i className='far fa-play-circle' />
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
-      </div> 
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
